d7a0e03c33f4497a73c4fff3a1fc865452b4cc83
Mamoun Hussein <mamoon.husen1006@gmail.com>
2021-06-10 15:41:07 +0300

    some problem in code

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -59,7 +59,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -77,15 +77,12 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-  let summation = 0;
-  for (let i = 0; i<sumArr.length; i++){
-    summation+=sumArr[i];
-  }
-  return [summation, `${sumArr} was passed in as an array of numbers, and ${summation} is their sum.`]
+
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-testSumArray(testArray);
+
+// testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
